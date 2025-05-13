2d71024fd91da3f548095105320cab7abc4cbdf0
Joshua Whiteurst <Jaywhiteurst@gmail.com>
2025-05-13 03:09:00 +0000

    encrpytion header and source

---
+++ Encryption.cpp
@@ -0,0 +1,10 @@
+#include "Encryption.h"
+
+std::string xorEncryptDecrypt(const std::string &data, const std::string &key) {
+    std::string result = data;
+    for (size_t i = 0; i < data.size(); i++)
+    {
+        /* code */
+    }
+    
+}
---
+++ Encryption.h
@@ -0,0 +1,8 @@
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
+
+#include <string>
+
+std::string xorEncryptDecrypt(const std::string &data, const std::string &key);
+
+#endif

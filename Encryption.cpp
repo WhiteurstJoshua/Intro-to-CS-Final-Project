e521ff5b78dc48a17251e9ae0a08a5f4909609a0
Joshua Whiteurst <Jaywhiteurst@gmail.com>
2025-05-13 03:12:01 +0000

    encrypt source

--- Encryption.cpp
+++ Encryption.cpp
@@ -2,9 +2,8 @@
 
 std::string xorEncryptDecrypt(const std::string &data, const std::string &key) {
     std::string result = data;
+
     for (size_t i = 0; i < data.size(); i++)
-    {
-        /* code */
-    }
-    
+        result[i] = data[i] ^ key[i  % key.size()];
+    return result
 }
